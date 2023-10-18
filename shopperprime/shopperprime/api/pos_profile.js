c2c9a9ef41d95d83b35471c25c9b06d84e89259d
Jawahar Ramkripal Mallah <jawahar.mallah@netmanthan.com>
2023-10-18 11:08:43 +0530

    ShoperPrime

--- shopperprime/shopperprime/api/pos_profile.js
+++ shopperprime/shopperprime/api/pos_profile.js
@@ -1,4 +1,4 @@
-// Copyright (c) 20201 Jawahar R Mallah and contributors
+// Copyright (c) 2023 Jawahar R Mallah and contributors
 // For license information, please see license.txt
 
 frappe.ui.form.on('POS Profile', {
